c5fcffb46d8ccba0cf704a630c1088b31bb8b7e2
mitya <dmitriy.akhmediyev@gmail.com>
2020-03-11 18:36:23 +0300

    music generation notebook

--- labyrinth/music_generator.py
+++ labyrinth/music_generator.py
@@ -1,22 +1,27 @@
-from pyo import *
 
-s = Server().boot()
-s.start()
+def generate_melody():
+    import pyo
 
-# 1
-# a = Sine(4400000, 2, 0.5).out()
+    s = Server().boot()
+    s.start()
+    mod = Sine(freq=50, mul=50)
+    mod2 = Sine(freq=100, mul=50)
 
-# 2
-# mod = Sine(freq=50,mul=50)
-# a = Sine(freq=mod+44000,mul=1).out()
+    t = CosTable([(0, 0), (100, 1), (500, .3), (8191, 0)])
+    beat = Beat(time=.125, taps=16, w1=[90, 80], w2=50, w3=35, poly=1).play()  # try taps=10 and larger poly
+    trmid = TrigXnoiseMidi(beat, dist=12, mrange=(60, 96))
+    trhz = Snap(trmid, choice=[0, 2, 3, 5, 7, 8, 10], scale=1)
+    tr2 = TrigEnv(beat, table=t, dur=beat['dur'], mul=beat['amp'])
 
-# 3
-# a = Sine(freq=mod+mod2+44000,mul=0.5).out()
+    mod = Sine(freq=50, mul=50)
+    mod2 = Sine(freq=100, mul=50)
+
+    a = Sine(freq=trhz + mod2, mul=tr2 * 0.2).out()
 
 
-mod = Sine(freq=50,mul=50)
-mod2 = Sine(freq=100,mul=50)
 
+
+""""# script #1
 a = Sine(freq=mod+mod2+4,mul=0.5).out()
 
 soundfile = SndTable(SNDS_PATH + "/transparent.aif")
@@ -28,8 +33,6 @@ comb1 = Delay(src, delay=[0.0297, 0.0277], feedback=0.65)
 comb2 = Delay(src, delay=[0.0371, 0.0393], feedback=0.51)
 comb3 = Delay(src, delay=[0.0411, 0.0409], feedback=0.5)
 comb4 = Delay(src, delay=[0.0137, 0.0155], feedback=0.73)
-
-
 
 # script#2
 # idea from http://ajaxsoundstudio.com/pyodoc/api/classes/triggers.html?highlight=beat#pyo.Beat
@@ -45,3 +48,14 @@ mod = Sine(freq=50,mul=50)
 mod2 = Sine(freq=100,mul=50)
 
 a = Sine(freq=trhz+mod2, mul=tr2*0.2).out()    # first try without modulations
+
+# 1
+# a = Sine(4400000, 2, 0.5).out()
+
+# 2
+# mod = Sine(freq=50,mul=50)
+# a = Sine(freq=mod+44000,mul=1).out()
+
+# 3
+# a = Sine(freq=mod+mod2+44000,mul=0.5).out()
+"""""""""""""""'"""'
